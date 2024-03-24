1d84c64e6c1fca847edd06e0648369e037945532
Nameisthinh <quocthinh00123@gmail.com>
2024-03-24 21:57:56 +0700

    Update

--- serverhop.lua
+++ serverhop.lua
@@ -13,14 +13,14 @@ end
 local function getServer()
 
     local success, decodedData = pcall(function()
-        return game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Asc&limit=20')).data
+        return game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Asc&limit=30')).data
     end)
 
     if not success then
         print("Error getting servers, using backup method")
         decodedData = game.HttpService:JSONDecode(alternateServersRequest()).data
     end
-    
+
     local servers = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26}
     local currentIndex = 1
 
