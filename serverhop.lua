37449d6da6f1fa1e9de08e9ca9d25772e1fc5ae2
Nameisthinh <quocthinh00123@gmail.com>
2024-03-25 03:27:27 +0700

    Fix

--- serverhop.lua
+++ serverhop.lua
@@ -30,7 +30,7 @@ local function getServer()
     -- else
     --     return getServer()
     -- end
-        local server = servers[Random.new():NextInteger(1, 1000)]
+        local server = servers[Random.new():NextInteger(5, 100)]
     
         if server then
             return server
