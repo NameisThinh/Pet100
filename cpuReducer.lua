c9d03dc535b3b1a2c0f5d1de8fa7ab00c05dc821
Nameisthinh <quocthinh00123@gmail.com>
2024-03-27 21:03:10 +0700

    Update Cpu

--- cpuReducer.lua
+++ cpuReducer.lua
@@ -1,6 +1,3 @@
-repeat
-  task.wait()
-until game:IsLoaded()
 
 local Workspace = game:GetService("Workspace")
 local Terrain = Workspace:WaitForChild("Terrain")
@@ -44,7 +41,7 @@ local function clearTextures(v)
   elseif v.Name == "Foilage" and v:IsA("Folder") then
       v:Destroy()
   elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
-      -- task.wait()
+      task.wait()
       v:Destroy()
   end
 end
