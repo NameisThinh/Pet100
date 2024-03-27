fb80183408f1baaed86e3a197ba8a92367e2b6f4
Nameisthinh <quocthinh00123@gmail.com>
2024-03-27 19:18:54 +0700

    Update

--- autoballoon.lua
+++ autoballoon.lua
@@ -83,10 +83,9 @@ spawn(function()
         wait()
     end
 end)
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/cpuReducer.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/cpuReducer.lua"))()
 while getgenv().autoBalloon do
     
-
     local balloonIds = {}
 
     local getActiveBalloons = ReplicatedStorage.Network.BalloonGifts_GetActiveBalloons:InvokeServer()
