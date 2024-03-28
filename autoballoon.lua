efc68b48b854729687b4b12bcffb253d01af350d
Nameisthinh <quocthinh00123@gmail.com>
2024-03-28 23:31:15 +0700

    Update balloon

--- autoballoon.lua
+++ autoballoon.lua
@@ -8,7 +8,7 @@ getgenv().autoBalloonConfig = {
     START_DELAY = 0, -- delay before starting
     SERVER_HOP_DELAY = 0, -- delay before server hopping
     BALLOON_DELAY = 0.5, -- delay before popping next balloon (if there are multiple balloons in the server)
-    GET_BALLOON_DELAY = 1, -- delay before getting balloons again if none are detected
+    GET_BALLOON_DELAY = 0.7, -- delay before getting balloons again if none are detected
     -- WAIT_FOR_BREAK = 1.5 -- delay in seconds to wait for the gift to break
     GIFT_BOX_BREAK_FAILSAFE = 1.5, -- seconds to wait before skipping gift boxes if they don't function properly
 }
@@ -22,62 +22,6 @@ local LocalPlayer = game:GetService("Players").LocalPlayer
 local breakables = game:GetService("Workspace"):WaitForChild("__THINGS"):WaitForChild("Breakables")
 local Client = ReplicatedStorage:WaitForChild("Library"):WaitForChild("Client")
 
-local Workspace = game:GetService("Workspace")
-local Terrain = Workspace:WaitForChild("Terrain")
-Terrain.WaterReflectance = 0
-Terrain.WaterTransparency = 1
-Terrain.WaterWaveSize = 0
-Terrain.WaterWaveSpeed = 0
-
-local Lighting = game:GetService("Lighting")
-Lighting.Brightness = 0
-Lighting.GlobalShadows = false
-Lighting.FogEnd = 9e100
-Lighting.FogStart = 0
-
-sethiddenproperty(Lighting, "Technology", 2)
-sethiddenproperty(Terrain, "Decoration", false)
-
-local function clearTextures(v)
-    if v:IsA("BasePart") and not v:IsA("MeshPart") then
-        v.Material = "Plastic"
-        v.Reflectance = 0
-    elseif (v:IsA("Decal") or v:IsA("Texture")) then
-        v.Transparency = 1
-    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-        v.Lifetime = NumberRange.new(0)
-    elseif v:IsA("Explosion") then
-        v.BlastPressure = 1
-        v.BlastRadius = 1
-    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-        v.Enabled = false
-    elseif v:IsA("MeshPart") then
-        v.Material = "Plastic"
-        v.Reflectance = 0
-        v.TextureID = 10385902758728957
-    elseif v:IsA("SpecialMesh")  then
-        v.TextureId = 0
-    elseif v:IsA("ShirtGraphic") then
-        v.Graphic = 1
-    elseif (v:IsA("Shirt") or v:IsA("Pants")) then
-        v[v.ClassName .. "Template"] = 1
-    elseif v.Name == "Foilage" and v:IsA("Folder") then
-        v:Destroy()
-    elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
-        task.wait()
-        v:Destroy()
-    end
-end
-
-game:GetService("Lighting"):ClearAllChildren()
-
-for _, v in pairs(Workspace:GetDescendants()) do
-    clearTextures(v)
-end
-
-Workspace.DescendantAdded:Connect(function(v)
-    clearTextures(v)
-end)
 
 
 pcall(function()
