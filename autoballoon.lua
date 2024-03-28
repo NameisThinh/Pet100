b6b622767ecd5c9e598e8b4b9aba97250bbfa982
Nameisthinh <quocthinh00123@gmail.com>
2024-03-29 06:08:37 +0700

    Update

--- autoballoon.lua
+++ autoballoon.lua
@@ -39,59 +39,59 @@ sethiddenproperty(Lighting, "Technology", 2)
 sethiddenproperty(Terrain, "Decoration", false)
 
 game:GetService("Lighting"):ClearAllChildren()
--- local function clearTextures(v)
---     if v:IsA("BasePart") and not v:IsA("MeshPart") then
---         v.Material = "Plastic"
---         v.Reflectance = 0
---     elseif (v:IsA("Decal") or v:IsA("Texture")) then
---         v.Transparency = 1
---         v:Destroy()
---     elseif v.Name == "Foilage" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "Foil" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "Wood" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "Sky" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "grass" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "ice" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "glass" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "Tree" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "Bush" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "Water" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "Brick" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "cobblestone" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v.Name == "woodplanks" and v:IsA("Folder") then
---         v:Destroy()
---     elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
---         v.Enabled = false
---     elseif v:IsA("SpecialMesh")  then
---         v.TextureId = 0
---     elseif v:IsA("ShirtGraphic") then
---         v:Destroy()
---     elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
---         v.Lifetime = NumberRange.new(0)
---     elseif v:IsA("Explosion") then
---         v.BlastPressure = 1
---         v.BlastRadius = 1
---     end
--- end
--- for _, v in pairs(Workspace:GetDescendants()) do
---     clearTextures(v)
--- end
+local function clearTextures(v)
+    if v:IsA("BasePart") and not v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+    elseif (v:IsA("Decal") or v:IsA("Texture")) then
+        v.Transparency = 1
+        v:Destroy()
+    elseif v.Name == "Foilage" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "Foil" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "Wood" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "Sky" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "grass" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "ice" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "glass" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "Tree" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "Bush" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "Water" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "Brick" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "cobblestone" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v.Name == "woodplanks" and v:IsA("Folder") then
+        v:Destroy()
+    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+        v.Enabled = false
+    elseif v:IsA("SpecialMesh")  then
+        v.TextureId = 0
+    elseif v:IsA("ShirtGraphic") then
+        v:Destroy()
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+        v.Lifetime = NumberRange.new(0)
+    elseif v:IsA("Explosion") then
+        v.BlastPressure = 1
+        v.BlastRadius = 1
+    end
+end
+for _, v in pairs(Workspace:GetDescendants()) do
+    clearTextures(v)
+end
 
--- Workspace.DescendantAdded:Connect(function(v)
---     clearTextures(v)
--- end)
+Workspace.DescendantAdded:Connect(function(v)
+    clearTextures(v)
+end)
 
 
 pcall(function()
