b059fe0376059492a2133ffb1a27b0bd01b43b45
Nameisthinh <quocthinh00123@gmail.com>
2024-03-28 23:46:00 +0700

    Update balloon

--- cpuReducer.lua
+++ cpuReducer.lua
@@ -1,5 +1,5 @@
 repeat
-  task.wait(3)
+  task.wait()
 until game:IsLoaded()
 
 local Workspace = game:GetService("Workspace")
@@ -21,39 +21,43 @@ sethiddenproperty(Lighting, "Technology", 2)
 sethiddenproperty(Terrain, "Decoration", false)
 
 local function clearTextures(v)
-    local className = v.ClassName
-    local name = v.Name
+  if v:IsA("BasePart") and not v:IsA("MeshPart") then
+      v.Material = "Plastic"
+      v.Reflectance = 0
 
-    if className == "BasePart" then
-        if not v:IsA("MeshPart") then
-            v.Material = "Plastic"
-            v.Reflectance = 0
-        end
-    elseif className == "Decal" or className == "Texture" then
-        v.Transparency = 1
-    elseif className == "ParticleEmitter" or className == "Trail" then
-        v.Lifetime = NumberRange.new(0)
-    elseif className == "Explosion" then
-        v.BlastPressure = 1
-        v.BlastRadius = 1
-    elseif className == "Fire" or className == "SpotLight" or className == "Smoke" or className == "Sparkles" then
-        v.Enabled = false
-    elseif className == "MeshPart" then
-        v.Material = "Plastic"
-        v.Reflectance = 0
-        v.TextureID = 10385902758728957
-    elseif className == "SpecialMesh" then
-        v.TextureId = 0
-    elseif className == "ShirtGraphic" then
-        v.Graphic = 1
-    elseif className == "Shirt" or className == "Pants" then
-        v[className .. "Template"] = 1
-    elseif name == "Foilage" and className == "Folder" then
-        v:Destroy()
-    elseif string.find(name, "Tree") or string.find(name, "Water") or string.find(name, "Bush") or string.find(name, "grass") then
-        task.wait()
-        v:Destroy()
-    end
+    v:Destroy()
+  elseif (v:IsA("Decal") or v:IsA("Texture")) then
+      v.Transparency = 1
+      v:Destroy()
+  elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+      v.Lifetime = NumberRange.new(0)
+  
+  elseif v:IsA("Explosion") then
+      v.BlastPressure = 1
+      v.BlastRadius = 1
+      v:Destroy()
+  elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+      v.Enabled = false
+      v:Destroy()
+  elseif v:IsA("MeshPart") then
+      v.Material = "Plastic"
+      v.Reflectance = 0
+      v.TextureID = 10385902758728957
+      v:Destroy()
+  elseif v:IsA("SpecialMesh")  then
+      v.TextureId = 0
+      v:Destroy()
+  elseif v:IsA("ShirtGraphic") then
+      v.Graphic = 1
+      v:Destroy()
+  elseif (v:IsA("Shirt") or v:IsA("Pants")) then
+      v[v.ClassName .. "Template"] = 1
+  elseif v.Name == "Foilage" and v:IsA("Folder") then
+      v:Destroy()
+  elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
+      task.wait()
+      v:Destroy()
+  end
 end
 
 game:GetService("Lighting"):ClearAllChildren()
