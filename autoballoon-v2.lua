6c30bf314a0e39ff9de812a46aaeb323f60489d1
Nameisthinh <quocthinh00123@gmail.com>
2024-04-01 05:17:37 +0700

    Update

--- autoballoon-v2.lua
+++ autoballoon-v2.lua
@@ -1,90 +1,8 @@
+-- MADE FIREDEVIL DO NOT SKID
+print("Made By firedevil (Ryan | 404678244215029762 | https://discord.gg/ettP4TjbAb)")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/waitForGameLoad.lua"))()
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/antiStaff.lua"))()
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/antiStaff.lua"))()
-
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local LocalPlayer = game:GetService("Players").LocalPlayer
-local breakables = game:GetService("Workspace"):WaitForChild("__THINGS"):WaitForChild("Breakables")
-local Client = ReplicatedStorage:WaitForChild("Library"):WaitForChild("Client")
-
-local Workspace = game:GetService("Workspace")
-local Terrain = Workspace:WaitForChild("Terrain")
-Terrain.WaterReflectance = 0
-Terrain.WaterTransparency = 1
-Terrain.WaterWaveSize = 0
-Terrain.WaterWaveSpeed = 0
-
-local Lighting = game:GetService("Lighting")
-Lighting.Brightness = 0
-Lighting.GlobalShadows = false
-Lighting.FogEnd = 9e100
-Lighting.FogStart = 0
-sethiddenproperty(Lighting, "Technology", 2)
-
-sethiddenproperty(Terrain, "Decoration", false)
-
-game:GetService("Lighting"):ClearAllChildren()
-local function clearTextures(v)
-    if v:IsA("BasePart") and not v:IsA("MeshPart") then
-        v.Material = "Plastic"
-        v.Reflectance = 0
-    elseif (v:IsA("Decal") or v:IsA("Texture")) then
-        v.Transparency = 1
-        v:Destroy()
-    elseif v.Name == "Foilage" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "Foil" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "Wood" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "Sky" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "grass" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "ice" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "glass" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "Tree" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "Bush" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "Water" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "Brick" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "cobblestone" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v.Name == "woodplanks" and v:IsA("Folder") then
-        v:Destroy()
-    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-        v.Enabled = false
-    elseif v:IsA("SpecialMesh")  then
-        v.TextureId = 0
-      elseif v:IsA("MeshPart") then
-        v.Material = "Plastic"
-        v.Reflectance = 0
-        v.TextureID = 10385902758728957
-    elseif v:IsA("ShirtGraphic") then
-      v.Graphic = 1
-    elseif (v:IsA("Shirt") or v:IsA("Pants")) then
-      v[v.ClassName .. "Template"] = 1
-    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-        v.Lifetime = NumberRange.new(0)
-    elseif v:IsA("Explosion") then
-        v.BlastPressure = 1
-        v.BlastRadius = 1
-    end
-end
-for _, v in pairs(Workspace:GetDescendants()) do
-    clearTextures(v)
-end
-
-Workspace.DescendantAdded:Connect(function(v)
-    clearTextures(v)
-end)
 
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -281,7 +199,8 @@ while getgenv().autoBalloon do
     end
 
     if (os.time() - startTimestamp) > getgenv().autoBalloonConfig.SERVER_MINIMUM_TIME then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/serverhop.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/serverhop.lua"))()
     end
 end
 -- MADE FIREDEVIL DO NOT SKID
+1
