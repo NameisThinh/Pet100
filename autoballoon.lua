7c461eb7925cbfd8a7dc442b1598909673c50c91
Nameisthinh <quocthinh00123@gmail.com>
2024-04-01 07:49:31 +0700

    Server

--- autoballoon.lua
+++ autoballoon.lua
@@ -184,7 +184,7 @@ while getgenv().autoBalloon do
                         if timeElapsed < getgenv().autoBalloonConfig.SERVER_MINIMUM_TIME then
                             task.wait(getgenv().autoBalloonConfig.SERVER_MINIMUM_TIME - timeElapsed)
                         end
-                        loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/serverhop.lua"))()
+                        loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/serverhopv1.lua"))()
                     end
                     break
                 end
@@ -229,7 +229,7 @@ while getgenv().autoBalloon do
             if timeElapsed < getgenv().autoBalloonConfig.SERVER_MINIMUM_TIME then
                 task.wait(getgenv().autoBalloonConfig.SERVER_MINIMUM_TIME - timeElapsed)
             end
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/serverhop.lua"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/serverhopv1.lua"))()
         end
 
         LocalPlayer.Character.HumanoidRootPart.Anchored = false
@@ -237,7 +237,7 @@ while getgenv().autoBalloon do
     end
 
     if (os.time() - startTimestamp) > getgenv().autoBalloonConfig.SERVER_MINIMUM_TIME then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/serverhop.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/nameisthinh/Pet100/thinh/serverhopv1.lua"))()
     end
 end
 
