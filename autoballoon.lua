a817bc5ad555ab3d3bfcb80726dfedb14703f26d
Nameisthinh <quocthinh00123@gmail.com>
2024-04-05 16:30:50 +0700

    fix

--- autoballoon.lua
+++ autoballoon.lua
@@ -6,7 +6,7 @@ getgenv().autoBalloon = true
 
 getgenv().autoBalloonConfig = {
     SERVER_HOP_AFTER_NOT_FIND = false, -- if the balloon isn't found, instead of checking through the rest of the balloons, it will just server hop
-    SERVER_MINIMUM_TIME = 30, -- minimum time to wait before server hopping
+    SERVER_MINIMUM_TIME = 50, -- minimum time to wait before server hopping
     START_DELAY = 1, -- delay before starting
     SERVER_HOP_DELAY = 0, -- delay before server hopping
     BALLOON_DELAY = 0.5, -- delay before popping next balloon (if there are multiple balloons in the server)
